b0f2057de790855794c2553f10936d8b97ed1b54
Ashley <106272446+Ashleyxbrianna@users.noreply.github.com>
2023-02-13 10:18:32 +0000

    added main photo on homepage

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./index.css";
+import mainPhoto from "../../../img/paperdragon.png";
 
 const Home = () => {
   return (
     <div className="page">
       <h1>DUNGEONS & DRAGONS</h1>
+      <img className="mainPhoto" src={mainPhoto} alt="paper dragon" />
     </div>
   );
 };
